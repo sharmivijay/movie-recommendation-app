b5baf5dfa7de6d8d5d411c4bd4770a7c36c62d09
sharmivijay <89096043+sharmivijay@users.noreply.github.com>
2021-09-26 16:26:43 +0530

    feat - Add movie recommendation list and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const animationList = [
+    { name: "Big Hero 6", rating: "10/10" },
+    { name: "Minion", rating: "9/10" }
+  ];
+
+  const fantasyList = [
+    { name: "Harry Potter Series", rating: "10/10" },
+    { name: "Night At the Museum", rating: "7/10" }
+  ];
+
+  const comedyList = [
+    { name: "Robin-B-Hood", rating: "10/10" },
+    { name: "Monster Hunt", rating: "9/10" }
+  ];
+
+  var [movies, setMovies] = useState(animationList);
+
+  function listAnimatedMovies() {
+    for (var i = 0; i < animationList.length; i++) {
+      setMovies(animationList);
+    }
+  }
+
+  function listFantasyMovies() {
+    for (var i = 0; i < fantasyList.length; i++) {
+      setMovies(fantasyList);
+    }
+  }
+
+  function listComedyMovies() {
+    for (var i = 0; i < comedyList.length; i++) {
+      setMovies(comedyList);
+    }
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>
+        <span role="img" aria-label="icon">
+          📽️
+        </span>{" "}
+        Rewatchable Movies
+      </h1>
+      <p>Select the below option to see my favorites </p>
+      <button className="btn" onClick={listAnimatedMovies}>
+        Animated
+      </button>
+      <button className="btn" onClick={listFantasyMovies}>
+        Fantasy
+      </button>
+      <button className="btn" onClick={listComedyMovies}>
+        Comedy
+      </button>
+      <hr className="line" />
+      <div>
+        {movies.map((movie) => {
+          return (
+            <div className="movieContainer">
+              <h5 className="movieName">{movie.name}</h5>
+              <br />
+              {movie.rating}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
